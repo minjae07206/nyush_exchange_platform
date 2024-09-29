f49743fb7bb81025d03c568a9ab3cfe100b6a593
Minjae <minjaekor@gmail.com>
2024-09-29 10:51:53 +0800

    added icons to NavigationBar

--- src/components/header/NavigationBar.tsx
+++ src/components/header/NavigationBar.tsx
@@ -6,14 +6,15 @@ interface NavigationBarProps {
 export default function NavigationBar ({className}:NavigationBarProps) {
     const location = useLocation();
     const pathname = location.pathname;
+    const commonliClass:string = "hover:bg-purple-600 p-2"
     return (
         <nav className={className}>
             <ul className="flex justify-around">
-                <li className={`${pathname === '/' ? "bg-purple-600" : ''} hover:bg-purple-600 border`}><Link to={'/'}>Home</Link></li>
-                <li className={`${pathname === '/new-post' ? "bg-purple-600" : ''} hover:bg-purple-600 border`}><Link to={'/new-post'}>Post</Link></li>
-                <li className={`${pathname === '/saved' ? "bg-purple-600" : ''} hover:bg-purple-600 border`}><Link to={'/saved'}>Saved</Link></li>
-                <li className={`${pathname === '/myposts' ? "bg-purple-600" : ''} hover:bg-purple-600 border`}><Link to={'/myposts'}>My Posts</Link></li>
-                <li className={`${pathname === '/settings' ? "bg-purple-600" : ''} hover:bg-purple-600 border`}><Link to={'/settings'}>Settings</Link></li>
+                <li className={`${pathname === '/' ? "bg-purple-600" : ''} ${commonliClass}`}><Link to={'/'}><i className="fa-solid fa-house pr-2"></i>Home</Link></li>
+                <li className={`${pathname === '/new-post' ? "bg-purple-600" : ''} ${commonliClass}`}><Link to={'/new-post'}><i className="fa-solid fa-square-plus pr-2"></i>Post</Link></li>
+                <li className={`${pathname === '/saved' ? "bg-purple-600" : ''} ${commonliClass}`}><Link to={'/saved'}><i className="fa-solid fa-bookmark pr-2"></i>Saved</Link></li>
+                <li className={`${pathname === '/myposts' ? "bg-purple-600" : ''} ${commonliClass}`}><Link to={'/myposts'}><i className="fa-solid fa-user pr-2"></i>My Posts</Link></li>
+                <li className={`${pathname === '/settings' ? "bg-purple-600" : ''} ${commonliClass}`}><Link to={'/settings'}><i className="fa-solid fa-gear pr-2"></i>Settings</Link></li>
             </ul>
         </nav>
     )
