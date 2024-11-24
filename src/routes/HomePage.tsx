34dc6e9a46990c3de78d9bd9b2b666a7d01b6ea3
Minjae <minjaekor@gmail.com>
2024-11-24 17:01:01 +0800

    configuring dockerfile

--- src/routes/HomePage.tsx
+++ src/routes/HomePage.tsx
@@ -3,6 +3,7 @@ export default function HomePage() {
         <div className="h-[1000px]">
             <h1>
             NYU Shanghai Community Exchange Platform
+            Welcome!
             </h1>
         </div>
     )
