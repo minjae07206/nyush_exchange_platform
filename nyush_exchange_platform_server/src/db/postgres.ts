411353b9ae0668a39f3864a2b536c0d442bcf54d
Minjae <minjaekor@gmail.com>
2024-12-05 14:38:53 +0800

    sdfds

--- nyush_exchange_platform_server/src/db/postgres.ts
+++ nyush_exchange_platform_server/src/db/postgres.ts
@@ -1,11 +1,9 @@
 import pg from 'pg';
 import dotenv from 'dotenv';
-let POSTGRES_HOST = "";
 if (process.env.NODE_ENV === 'production') {
     dotenv.config({ path: '/home/ml6722/.env' });  // Path for production
 } else {
     dotenv.config({ path: '../.env' });  // Default, loads from the root .env file for development
-    POSTGRES_HOST = "localhost"
 }// Load environment variables from .env file
 const { Pool } = pg;
 console.log(process.env.POSTGRES_HOST)
@@ -13,7 +11,7 @@ console.log(process.env.POSTGRES_HOST)
 
 const pool = new Pool(
     {
-        host: POSTGRES_HOST,
+        host: process.env.NODE_ENV === 'production'? process.env.POSTGRES_HOST : "localhost",
         user: process.env.POSTGRES_USER,
         database: process.env.POSTGRES_DATABASE,
         password: process.env.POSTGRES_PASSWORD,
