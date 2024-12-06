8fd846c4215485ecd6f5ad001d90232f48ab3b46
Minjae <minjaekor@gmail.com>
2024-12-06 13:12:43 +0800

    try this

--- nyush_exchange_platform_server/src/api/post/create-post.ts
+++ nyush_exchange_platform_server/src/api/post/create-post.ts
@@ -6,7 +6,7 @@ import { v4 as uuidv4 } from 'uuid';
 import { readFileSync } from 'fs'; // Importing the file system module
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, '/nyush_exchange_platform_server/var/www/uploads');
+      cb(null, '/uploads');
     },
     filename: function (req, file, cb) {
       const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
