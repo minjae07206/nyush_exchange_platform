6249910c1dece078ded06f120bb3d54d95e822b2
Minjae <minjaekor@gmail.com>
2024-12-06 13:37:55 +0800

    lets go

--- nyush_exchange_platform_server/src/api/post/create-post.ts
+++ nyush_exchange_platform_server/src/api/post/create-post.ts
@@ -6,7 +6,8 @@ import { v4 as uuidv4 } from 'uuid';
 import { readFileSync } from 'fs'; // Importing the file system module
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, '/uploads');
+        const uploadPath = path.resolve(__dirname, 'nyush_exchange_platform_server', 'var', 'www', 'uploads');
+        cb(null, uploadPath);
     },
     filename: function (req, file, cb) {
       const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
