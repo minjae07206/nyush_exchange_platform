c7cc657241de5e34127f31f3dbb041af54f0f672
Minjae <minjaekor@gmail.com>
2025-04-19 12:31:54 +0800

    archive not working

--- nyush_exchange_platform_server/src/api/post/archive-post.ts
+++ nyush_exchange_platform_server/src/api/post/archive-post.ts
@@ -6,9 +6,11 @@ const router = express.Router();
 
 router.patch('/', async (req: Request, res: Response) => {
     const { postId } = req.body;
+    console.log(postId);
     try {
         const filePath = path.join(__dirname, '..', '..', '..', 'src', 'sql_queries', 'archive_post.sql');
         const archive_post_query = readFileSync(filePath, 'utf-8');
+        console.log("archiving...")
         await pool.query(archive_post_query, [postId]);
         res.status(200).json({ message: 'Post successfully archived' });
     } catch (error) {
