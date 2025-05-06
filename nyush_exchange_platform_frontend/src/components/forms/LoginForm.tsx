fc8f2224a5879682f8972aab1f65459745e72a81
Minjae <minjaekor@gmail.com>
2025-05-06 21:59:54 +0800

    testing without HOSTNAME for the frontend

--- nyush_exchange_platform_frontend/src/components/forms/LoginForm.tsx
+++ nyush_exchange_platform_frontend/src/components/forms/LoginForm.tsx
@@ -64,7 +64,7 @@ export default function LoginForm() {
 
         if (CAN_PROCEED_TO_MAKING_POST_REQUEST) {
             console.log(process.env.REACT_APP_HOST_NAME);
-            axios.post(`${process.env.REACT_APP_HOST_NAME}/api/auth/login`, {
+            axios.post(`/api/auth/login`, {
                 usernameOrEmail: usernameOrEmailInput,
                 password: passwordInput,
             }).then((response) => {
