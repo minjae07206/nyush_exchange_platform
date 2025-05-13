81ba9382d80d86d977fec1592b80ccb744b30c35
Minjae <minjaekor@gmail.com>
2025-05-13 23:11:16 +0800

    trying to fix bug

--- nyush_exchange_platform_server/src/api/auth/check-login.ts
+++ nyush_exchange_platform_server/src/api/auth/check-login.ts
@@ -2,10 +2,10 @@ import express, { Request, Response } from 'express';
 const router = express.Router();
 
 router.get('/', async (req: Request, res: Response) => {
-    console.log("Request:", req)
-    console.log(req.session.user);
+    console.log("COOKIE", req.session.cookie);
+    console.log("req.session.user", req.session.user)
     // It turns out I don't need to check the redis sessionDB, req.session.user is already checking the db for me.
-    if (req.session.user) {
+    if (req.session.cookie) {
         res.status(200).json({message:'session-alive'});
     } else {
         // User is not logged in
