96f79f9a9a2a4a3317dfd1c0619742c3cbd8af35
Minjae <minjaekor@gmail.com>
2025-05-13 21:58:40 +0800

    debugging with req.session.user

--- nyush_exchange_platform_server/src/api/auth/check-login.ts
+++ nyush_exchange_platform_server/src/api/auth/check-login.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from 'express';
 const router = express.Router();
 
 router.get('/', async (req: Request, res: Response) => {
+    console.log(req.session.user);
     // It turns out I don't need to check the redis sessionDB, req.session.user is already checking the db for me.
     if (req.session.user) {
         res.status(200).json({message:'session-alive'});
